c2b89eecc3ca940b8dab0f55d4e57be6edd364d4
Sergei <sergei@SERGEI-LAPTOP>
2020-03-08 19:44:28 +0300

    Добавьте файлы проекта.

---
+++ DSPEmulator/ChannelsDelaySampleProvider.cs
@@ -0,0 +1,160 @@
+﻿using NAudio.Wave;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace DSPEmulator
+{
+    class ChannelsDelaySampleProvider : ISampleProvider
+    {
+        private readonly ISampleProvider sourceProvider;
+        private float[] sourceBuffer;
+        private List<float> leftChannel = new List<float>(),
+            rightChannel = new List<float>();
+        private List<float> leftChannelPrevious = new List<float>(),
+            rightChannelPrevious = new List<float>();
+        private int leftDelayBySamples;
+        private int rightDelayBySamples;
+
+        public ChannelsDelaySampleProvider(ISampleProvider sourceProvider)
+        {
+            if (sourceProvider.WaveFormat.Channels != 2) throw new InvalidOperationException("Not a stereo audio file.");
+
+            this.sourceProvider = sourceProvider;
+        }
+        public TimeSpan LeftDelay
+        {
+            get { return SamplesToTimeSpan(leftDelayBySamples); }
+            set { leftDelayBySamples = TimeSpanToSamples(value); }
+        }
+
+        public TimeSpan RightDelay
+        {
+            get { return SamplesToTimeSpan(rightDelayBySamples); }
+            set { rightDelayBySamples = TimeSpanToSamples(value); }
+        }
+
+        public int LeftDelayBySamples
+        {
+            get { return leftDelayBySamples; }
+            set { leftDelayBySamples = value; }
+        }
+
+        public int RightDelayBySamples
+        {
+            get { return rightDelayBySamples; }
+            set { rightDelayBySamples = value; }
+        }
+
+        private int TimeSpanToSamples(TimeSpan time)
+        {
+            var samples = (int)(time.TotalSeconds * WaveFormat.SampleRate);
+            return samples;
+        }
+
+        private TimeSpan SamplesToTimeSpan(int samples)
+        {
+            return TimeSpan.FromSeconds(samples / (double)WaveFormat.SampleRate);
+        }
+
+        public WaveFormat WaveFormat => sourceProvider.WaveFormat;
+
+        public int Read(float[] buffer, int offset, int count)
+        {
+            EnsureSourceBuffer(count);
+            var samlesReadedFromSource = sourceProvider.Read(sourceBuffer, offset, count);
+
+            if (Math.Max(leftChannelPrevious.Count, rightChannelPrevious.Count) == 0 && samlesReadedFromSource == 0) return 0;
+
+            channelize(sourceBuffer, samlesReadedFromSource);
+
+            delayChannel(leftChannel, leftDelayBySamples);
+            delayChannel(rightChannel, rightDelayBySamples);
+
+            insertFromPrevious(leftChannel, leftChannelPrevious);
+            insertFromPrevious(rightChannel, rightChannelPrevious);
+
+            if (samlesReadedFromSource != 0)
+            {
+                fillPreviousSamples(leftChannel, leftChannelPrevious, leftDelayBySamples);
+                fillPreviousSamples(rightChannel, rightChannelPrevious, rightDelayBySamples);
+
+                leftChannel.RemoveRange(leftChannel.Count - leftDelayBySamples, leftDelayBySamples);
+                rightChannel.RemoveRange(rightChannel.Count - rightDelayBySamples, rightDelayBySamples);
+            }
+
+            var stereoBuffer = channelsToBuffer(leftChannel, rightChannel);
+
+            for(int i = 0; i < stereoBuffer.Length; ++i)
+            {
+                buffer[i] = stereoBuffer[i];
+            }
+
+            return stereoBuffer.Length;
+        }
+
+        private float[] channelsToBuffer(List<float> leftChannel, List<float> rightChannel)
+        {
+            var resultBuffer = new List<float>(Math.Max(leftChannel.Count, rightChannel.Count) * 2);
+
+            for(int i = 0; i < Math.Max(leftChannel.Count, rightChannel.Count); ++i)
+            {
+                if(i < leftChannel.Count)
+                resultBuffer.Add(leftChannel[i]);
+                else resultBuffer.Add(0);
+
+                if(i < rightChannel.Count)
+                resultBuffer.Add(rightChannel[i]);
+                else resultBuffer.Add(0);
+            }
+
+            return resultBuffer.ToArray();
+        }
+
+        private void fillPreviousSamples(List<float> list, List<float> previousList, int endSamples)
+        {
+            for (int i = list.Count - endSamples; i < list.Count; ++i)
+            {
+                previousList.Add(list[i]);
+            }
+        }
+
+        private void insertFromPrevious(List<float> list, List<float> previousList)
+        {
+            for (int i = 0; i < previousList.Count; ++i)
+            {
+                list[i] = previousList[i];
+            }
+            previousList.Clear();
+        }
+
+        private void delayChannel(List<float> list, int samples)
+        {
+           for(int i = 0; i < samples; ++i)
+            {
+                list.Insert(0, 0);
+            }
+        }
+
+        private void channelize(float[] sourceBuffer, int length)
+        {
+            leftChannel.Clear();
+            rightChannel.Clear();
+
+            for(int i = 0; i < length; i+=2)
+            {
+                leftChannel.Add(sourceBuffer[i]);
+                rightChannel.Add(sourceBuffer[i + 1]);
+            }
+        }
+
+        private void EnsureSourceBuffer(int count)
+        {
+            if (sourceBuffer == null || sourceBuffer.Length < count)
+            {
+                sourceBuffer = new float[count];
+            }
+        }
+    }
+}
---
+++ DSPEmulator/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using NAudio;
+using NAudio.Wave;
+using NAudio.MediaFoundation;
+using NAudio.Wave.SampleProviders;
+
+namespace DSPEmulator
+{
+    class Program
+    {
+        private static string outputDir = "OUTPUT";
+        static void Main(string[] args)
+        {
+            MediaFoundationApi.Startup();
+            Directory.CreateDirectory(Path.Combine(Environment.CurrentDirectory, outputDir));
+
+            foreach (var arg in args)
+            {
+                try
+                {
+                    Console.WriteLine($"Loading file {Path.GetFileName(arg)}.");
+                    var loadedAudio = LoadFile(arg);
+
+                    var stereo = loadedAudio;
+                    if (loadedAudio.WaveFormat.Channels != 2)
+                    {
+                        stereo = new MonoToStereoSampleProvider(loadedAudio);
+                    }
+
+                    ISampleProvider resampled = stereo;
+                    if(loadedAudio.WaveFormat.SampleRate != 44100)
+                    {
+                        Console.WriteLine($"Resampling to 44100...");
+                        resampled = new WdlResamplingSampleProvider(loadedAudio, 44100);
+                    }
+
+                    Console.WriteLine("Processing...");
+                    var processedAudio = ChannelsDelay(resampled, 1000, 0);
+
+                    Console.WriteLine("Saving to mp3...");
+                    saveToMp3(Path.Combine(Environment.CurrentDirectory, outputDir, $"{Path.GetFileNameWithoutExtension(arg)}.mp3"),
+                        processedAudio);
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine($"Error on file: {Path.GetFileName(arg)}. {ex.Message}");
+                }
+            }
+            Console.WriteLine("Done.");
+            Console.ReadLine();
+        }
+
+
+        private static ISampleProvider LoadFile(string arg)
+        {
+            var audioFile = new AudioFileReader(arg);
+
+            Console.WriteLine($"{audioFile.WaveFormat.SampleRate} {audioFile.WaveFormat.BitsPerSample} {audioFile.WaveFormat.Channels}");
+
+            return audioFile;
+
+
+        }
+
+        private static ISampleProvider ChannelsDelay(ISampleProvider audio, double leftChannelDelayMillisec, double rightChannelDelayMillisec)
+        {
+            return new ChannelsDelaySampleProvider(audio)
+            {
+                LeftDelay = TimeSpan.FromMilliseconds(leftChannelDelayMillisec),
+                RightDelay = TimeSpan.FromMilliseconds(rightChannelDelayMillisec)
+            };
+        }
+
+        private static void saveToMp3(string fileName, ISampleProvider audioToSave)
+        {
+            MediaFoundationEncoder.EncodeToMp3(audioToSave.ToWaveProvider(), fileName, 320000);
+        }
+    }
+}
