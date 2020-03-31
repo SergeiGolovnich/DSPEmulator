1860953cc9428a72e43f27ba8edf110e39978480
Sergei <sergei@SERGEI-LAPTOP>
2020-03-31 20:38:17 +0300

    Added channel equalizer

---
+++ DSPEmulator/EqualizerBand.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DSPEmulator
+{
+    class EqualizerBand
+    {
+        public float Frequency { get; set; }
+        public float Gain { get; set; }
+        public float Bandwidth { get; set; }
+    }
+}
