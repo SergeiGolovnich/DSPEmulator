74ddf89703e352d0faf3d59219fa3e8521e42550
Sergei <sergei@SERGEI-LAPTOP>
2020-04-30 18:57:58 +0300

    Create project for UI

---
+++ DSPEmulatorUI/Bootstrapper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using Caliburn.Micro;
+using DSPEmulatorUI.ViewModels;
+
+namespace DSPEmulatorUI
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<MainWindowViewModel>();
+        }
+    }
+}
---
+++ DSPEmulatorUI/Views/MainWindowView.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DSPEmulatorUI.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindowView.xaml
+    /// </summary>
+    public partial class MainWindowView : Window
+    {
+        public MainWindowView()
+        {
+            InitializeComponent();
+        }
+    }
+}
