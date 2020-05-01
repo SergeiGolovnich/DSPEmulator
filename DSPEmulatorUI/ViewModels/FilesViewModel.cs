71f39391cede031a1367e5575ae6f0db30262390
Sergei <sergei@SERGEI-LAPTOP>
2020-05-01 15:11:49 +0300

    Can remove files from listbox

--- DSPEmulatorUI/ViewModels/FilesViewModel.cs
+++ DSPEmulatorUI/ViewModels/FilesViewModel.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Input;
 
 namespace DSPEmulatorUI.ViewModels
 {
@@ -12,6 +13,7 @@ namespace DSPEmulatorUI.ViewModels
         {
             DisplayName = "Audio Files";
         }
+        public string SelectedFile { get; set; }
         public BindableCollection<string> Files { get; set; } = new BindableCollection<string>();
         public string OutputFolder { get; set; } = Environment.GetFolderPath(Environment.SpecialFolder.MyMusic);
 
@@ -19,8 +21,8 @@ namespace DSPEmulatorUI.ViewModels
         {
             var dialog = new Microsoft.Win32.OpenFileDialog();
 
-            dialog.Filter = "Audio Files| *.flac, *.mp3";
-            dialog.Title = "Select audio files";
+            dialog.Filter = "Audio Files| *.flac;*.mp3";
+            dialog.Title = "Select Audio Files";
             dialog.CheckFileExists = true;
             dialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyMusic);
             dialog.Multiselect = true;
@@ -34,6 +36,40 @@ namespace DSPEmulatorUI.ViewModels
                         Files.Add(filePath);
                     }
                 }
+            }
+        }
+
+        public void SetOutputFolder()
+        {
+            // Create a "Save As" dialog for selecting a directory (HACK)
+            var dialog = new Microsoft.Win32.SaveFileDialog();
+
+            dialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyMusic); // Use current value for initial dir
+            dialog.Title = "Select a Directory"; // instead of default "Save As"
+            dialog.Filter = "Directory|*.this.directory"; // Prevents displaying files
+            dialog.FileName = "select"; // Filename will then be "select.this.directory"
+            if (dialog.ShowDialog() == true)
+            {
+                string path = dialog.FileName;
+                // Remove fake filename from resulting path
+                path = path.Replace("\\select.this.directory", "");
+                path = path.Replace(".this.directory", "");
+                // If user has changed the filename, create the new directory
+                if (!System.IO.Directory.Exists(path))
+                {
+                    System.IO.Directory.CreateDirectory(path);
+                }
+                // Our final value is in path
+                OutputFolder = path;
+                NotifyOfPropertyChange(() => OutputFolder);
+            }
+        }
+
+        public void RemoveSelectedItem(object item, KeyEventArgs e)
+        {
+            if (e.Key == Key.Delete && item != null)
+            {
+                Files.Remove((string)item);
             }
         }
     }
--- DSPEmulatorUI/Views/FilesView.xaml.cs
+++ DSPEmulatorUI/Views/FilesView.xaml.cs
@@ -22,5 +22,6 @@ namespace DSPEmulatorUI.Views
         {
             InitializeComponent();
         }
+
     }
 }
