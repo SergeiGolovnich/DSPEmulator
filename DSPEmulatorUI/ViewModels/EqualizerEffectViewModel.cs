67dece2e57c3340d995fc24a9cda6fe1f9cb869b
Sergei <sergei@SERGEI-LAPTOP>
2020-05-07 14:40:35 +0300

    Add volume adjustment to equalizer

--- DSPEmulatorUI/ViewModels/EqualizerEffectViewModel.cs
+++ DSPEmulatorUI/ViewModels/EqualizerEffectViewModel.cs
@@ -18,9 +18,35 @@ namespace DSPEmulatorUI.ViewModels
         }
         public ISampleProvider EffectProvider(ISampleProvider sourceProvider)
         {
-            return new EqualizerSampleProvider(sourceProvider,
-                ((EqualizerChannelViewModel)Items[0]).EqualizerBands.ToArray(),
-                ((EqualizerChannelViewModel)Items[1]).EqualizerBands.ToArray());
+            var eqParams = new EqualizerParams()
+            {
+                LeftChannel = ((EqualizerChannelViewModel)Items[0]).EqualizerBands,
+                RightChannel = ((EqualizerChannelViewModel)Items[1]).EqualizerBands
+            };
+
+            float adjustVolume = calcAdjustVolumeFromEq(eqParams);
+            var adjusted = new ChannelsVolumeSampleProvider(sourceProvider)
+            {
+                LeftChannelVolumeInDB = adjustVolume,
+                RightChannelVolumeInDB = adjustVolume
+            };
+
+            return new EqualizerSampleProvider(adjusted, eqParams);
+        }
+
+        private float calcAdjustVolumeFromEq(EqualizerParams eqParams)
+        {
+            float maxEqGain = 0;
+            foreach (EqualizerBand eb in eqParams.LeftChannel)
+            {
+                if (eb.Gain > 0) maxEqGain = Math.Max(eb.Gain, maxEqGain);
+            }
+            foreach (EqualizerBand eb in eqParams.RightChannel)
+            {
+                if (eb.Gain > 0) maxEqGain = Math.Max(eb.Gain, maxEqGain);
+            }
+
+            return -maxEqGain;
         }
     }
 }
