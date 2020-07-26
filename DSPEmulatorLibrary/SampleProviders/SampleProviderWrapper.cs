44a3b33417376b6559f5f5eff33c542ec7b4ed62
Sergei <sergei@SERGEI-LAPTOP>
2020-07-26 15:04:20 +0300

    Added class SampleProviderWrapper

---
+++ DSPEmulatorLibrary/SampleProviders/SampleProviderWrapper.cs
@@ -0,0 +1,21 @@
+﻿using NAudio.Wave;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DSPEmulatorLibrary.SampleProviders
+{
+    class SampleProviderWrapper : ISampleProvider
+    {
+        public ISampleProvider SourceProvider { get; set; }
+        public WaveFormat WaveFormat => SourceProvider.WaveFormat;
+        public SampleProviderWrapper(ISampleProvider sourceProvider)
+        {
+            SourceProvider = sourceProvider;
+        }
+        public int Read(float[] buffer, int offset, int count)
+        {
+            return SourceProvider.Read(buffer, offset, count);
+        }
+    }
+}
