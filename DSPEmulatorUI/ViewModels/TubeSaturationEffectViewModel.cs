fc47a120397333133a97b99808ac5ec45fc4921b
Sergei <sergei@SERGEI-LAPTOP>
2020-09-24 15:34:30 +0300

    Сreated an empty view and viewmodel

---
+++ DSPEmulatorUI/ViewModels/TubeSaturationEffectViewModel.cs
@@ -0,0 +1,29 @@
+﻿using Caliburn.Micro;
+using DSPEmulatorLibrary;
+using DSPEmulatorLibrary.SampleProviders;
+using NAudio.Wave;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace DSPEmulatorUI.ViewModels
+{
+    [JsonObject(MemberSerialization.OptIn)]
+    class TubeSaturationEffectViewModel : Screen, IEffect
+    {
+        [JsonProperty()]
+        public string EffectType => typeof(TubeSaturationEffectViewModel).Name;
+
+        public string EffectDisplayName => "Tube Saturation";
+
+        public TubeSaturationEffectViewModel() { }
+        public TubeSaturationEffectViewModel(JToken jToken)
+        {
+
+        }
+
+        public ISampleProvider SampleProvider(ISampleProvider sourceProvider)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
