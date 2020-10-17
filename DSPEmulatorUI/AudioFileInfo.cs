b36c9caccb03a10d9eeb9abec7d26dbcbf5b7fba
Sergei <sergei@SERGEI-LAPTOP>
2020-10-17 18:29:48 +0300

    Added AudioFileInfo class

---
+++ DSPEmulatorUI/AudioFileInfo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DSPEmulatorUI
+{
+    public class AudioFileInfo
+    {
+        public string OriginFolder { get; set; }
+        public string FullPath { get; set; }
+    }
+}
